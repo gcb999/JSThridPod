7c318f7f84eeaa5c743a849ceb1b076a222f450a
gcb999 <546611623@qq.com>
2016-04-29 09:56:52 +0800

    修改横栏控件

--- JSThridPod/Demo用法/网易横栏的用法/WYController.m
+++ JSThridPod/Demo用法/网易横栏的用法/WYController.m
@@ -9,24 +9,72 @@
 #import "WYController.h"
 #import "JSNinaPagerViewConfig.h"
 
-@interface WYController ()<JSNinaPagerViewConfigDelegate>
+@interface WYController ()<NinaPagerViewDelegate>
 
 @end
 
 
 @implementation WYController
 
+
+#pragma mark -标题集合
+
+-(NSArray *)titles{
+    
+    NSArray *titleArray =   @[
+                              @"大连市",
+                              @"甘井子",
+                              @"星海广场",
+                              @"西岗",
+                              @"马栏子",
+                              @"革镇堡",
+                              @"中山",
+                              @"人民广场",
+                              @"中山广场"
+                              ];
+    
+    
+    
+    
+    return titleArray;
+}
+
+
+#pragma mark -代码控制器集合
+
+-(NSArray *)Controllers{
+    
+    NSArray *vcsArray = @[
+                          @"NormalTableViewController",
+                          @"HeaderAnimationTableViewViewController",
+                          @"NormalCollecionViewController",
+                          @"FlowOutCollectionViewController",
+                          @"JSTabbarViewController",
+                          @"FirstViewController",
+                          @"FlowOutCollectionViewController",
+                          @"NormalTableViewController",
+                          @"HeaderAnimationTableViewViewController",
+                          ];
+    
+    return vcsArray;
+    
+}
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.title=@"网易横栏的用法";
     CGRect rect = CGRectMake(0, 44, IPHONScreenWidth, IPHONScreenHeight-44);
-    [[JSNinaPagerViewConfig share]initWithFrame:rect delegate:self];
+    JSNinaPagerViewConfig *config=[[JSNinaPagerViewConfig alloc] initWithFrame:rect titles:[self titles] vcs:[self Controllers] delegate:self];
+    [config loadingConfig];
+    [self.view addSubview:config];
   
     
 }
 
 -(BOOL)deallocVCsIfUnnecessary{
-    return NO;
+    return YES;
 }
 @end
--- JSThridPod/ShareControl/UI/JSNinaPagerViewConfig/JSNinaPagerViewConfig.m
+++ JSThridPod/ShareControl/UI/JSNinaPagerViewConfig/JSNinaPagerViewConfig.m
@@ -14,7 +14,7 @@
 @interface JSNinaPagerViewConfig()
 
 {
-    id _delegate;
+ 
 }
 
 @end
@@ -23,69 +23,62 @@
 @implementation JSNinaPagerViewConfig
 
 
-+(JSNinaPagerViewConfig*)share{
-    static dispatch_once_t pred = 0;
-    __strong static id _sharedObject = nil;
-    dispatch_once(&pred, ^{
-        _sharedObject = [[self alloc] init]; // or some other init method
+
+
+
+- (instancetype)initWithFrame:(CGRect)frame  titles:(NSArray *)titles  vcs:(NSArray *)vcs  delegate:(id<NinaPagerViewDelegate>)delegate
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+       
+        //设置默认颜色
+        self.colors=[self colors];
         
-    });
-    return _sharedObject;
+        self.titles=titles;
+        
+        self.vcs=vcs;
+        
+        self.delegate=delegate;
+        
+        
+       
+    }
+    return self;
 }
 
-
-/**< 每个标题下对应的控制器，只需将您创建的控制器类名加入下列数组即可(注意:数量应与上方的titles数量保持一致，若少于titles数量，
- 
- 下方会打印您缺少相应的控制器，同时默认设置的最大控制器数量为10)
- 
- **/
-
-
-
-#pragma mark -标题集合
-
--(NSArray *)titles{
+-(void)loadingConfig{
     
-    NSArray *titleArray =   @[
-                              @"大连市",
-                              @"甘井子",
-                              @"星海广场",
-                              @"西岗",
-                              @"马栏子",
-                              @"革镇堡",
-                              @"中山",
-                              @"人民广场",
-                              @"中山广场"
-                              ];
+    /**<
+     
+     创建ninaPagerView，控制器第一次是根据您划的位置进行相应的添加的，类似网易新闻虎扑看球等的效果，后面再滑动到相应位置时不再重新添加，如果想刷新数据，您可以在相应的控制器里加入刷新功能，低耦合。需要注意的是，在创建您的控制器时，设置的
+     
+     frame为FUll_CONTENT_HEIGHT，即全屏高减去导航栏高度再减去Tabbar的高度，如果这个高度不是您想要的，您可以去UIParameter.h中进行设置XD。
+     
+     **/
+    self.ninaPagerView = [[NinaPagerView alloc] initWithTitles:self.titles WithVCs:self.vcs WithColorArrays:self.colors];
+    self.ninaPagerView.frame=self.bounds;
+    /**<  Tag0.5.2 添加:您可以设置titleSize这个属性来设置标题的缩放比例(相对于原比例标题)，推荐您设置的范围在1~1.5之间，如果不设置此属性，默认的缩放比例为1.15。
+     Tag0.5.2 Add:You can set titleSize for title animation(compare to origin title),command range between 1 and 1.5.If don't set this,default scale is 1.15. **/
+    //    ninaPagerView.titleScale = 1.5;
     
- 
-      
-      
-    return titleArray;
-}
-
-
-#pragma mark -代码控制器集合
-
--(NSArray *)Controllers{
     
-    NSArray *vcsArray = @[
-                          @"NormalTableViewController",
-                          @"HeaderAnimationTableViewViewController",
-                          @"NormalCollecionViewController",
-                          @"FlowOutCollectionViewController",
-                          @"JSTabbarViewController",
-                          @"FirstViewController",
-                          @"FlowOutCollectionViewController",
-                          @"NormalTableViewController",
-                          @"HeaderAnimationTableViewViewController",
-                          ];
+    self.ninaPagerView.delegate = self.delegate;
     
-    return vcsArray;
+    [self addSubview:self.ninaPagerView];
+    
+
+    
+    
     
 }
 
-
+-(void)layoutSubviews{
+    [super layoutSubviews];
+    /**< 因为在框架里设置的问题，所以您如果需要在第一个控制器中需要push到二级控制器的话，需要多加上下方的这行代码，并且
+     此行代码需要在addSubView的后面。
+     Because of the framework setting,you must add following codes under the line after addSubView codes in order to push second level VC. **/
+    self.ninaPagerView.pushEnabled=YES;
+}
 
 
 
@@ -118,41 +111,6 @@
 
 
 
-
--(void)initWithFrame:(CGRect)frame  delegate:(id<JSNinaPagerViewConfigDelegate>)delegate{
-    
-    /**< 
-     
-     创建ninaPagerView，控制器第一次是根据您划的位置进行相应的添加的，类似网易新闻虎扑看球等的效果，后面再滑动到相应位置时不再重新添加，如果想刷新数据，您可以在相应的控制器里加入刷新功能，低耦合。需要注意的是，在创建您的控制器时，设置的
-     
-     frame为FUll_CONTENT_HEIGHT，即全屏高减去导航栏高度再减去Tabbar的高度，如果这个高度不是您想要的，您可以去UIParameter.h中进行设置XD。
-     
-     **/
-    
-    
-    NinaPagerView *ninaPagerView = [[NinaPagerView alloc] initWithTitles:[self titles] WithVCs:[self Controllers] WithColorArrays:[self colors]];
-    ninaPagerView.frame=frame;
-    /**<  Tag0.5.2 添加:您可以设置titleSize这个属性来设置标题的缩放比例(相对于原比例标题)，推荐您设置的范围在1~1.5之间，如果不设置此属性，默认的缩放比例为1.15。
-     Tag0.5.2 Add:You can set titleSize for title animation(compare to origin title),command range between 1 and 1.5.If don't set this,default scale is 1.15. **/
-    //    ninaPagerView.titleScale = 1.5;
-    
-    ninaPagerView.delegate = delegate;
-    
-    if ([delegate isKindOfClass:[UIViewController class]]) {
-        UIViewController *vc=(UIViewController *)delegate;
-        [vc.view addSubview:ninaPagerView];
-    }
-    else if ([delegate isKindOfClass:[UIView class]]){
-        UIView *vw=(UIView *)delegate;
-        [vw addSubview:ninaPagerView];
-    }
-  
-    /**< 因为在框架里设置的问题，所以您如果需要在第一个控制器中需要push到二级控制器的话，需要多加上下方的这行代码，并且
-     此行代码需要在addSubView的后面。
-     Because of the framework setting,you must add following codes under the line after addSubView codes in order to push second level VC. **/
-    ninaPagerView.pushEnabled=YES;
-  
-}
 
 
 
